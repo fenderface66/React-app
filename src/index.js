ff2d06dd3893f19e0c02bc594f86ec856c145d44
Robbie Heygate <r.heygate.dev@gmail.com>
2017-05-10 15:31:06 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyATaOU7T1EOZz97_ETFAjmF8R0LZkX1sXI';
+
+//Render component's generated HTML
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  //Declare root element of app
+  , document.querySelector('.container'));
