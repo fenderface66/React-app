9ebf08875bd1d1c2b0b8952e6b378274b0979905
fenderface66 <r.heygate@gmail.com>
2017-06-06 20:43:49 +0100

    Base project finished

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,32 @@
 import React, {	Component	} from 'react';
 
-class SearchBar extends Component {
+class SearchBar extends Component { 
+	constructor(props) { 
+		
+		super(props);
+		
+		this.state = { term: 'Tester'};
+		
+	}
+	
 	render() {
-		return <input onChange={(event) => console.log(event.target.value)} />;
+		
+		return (
+			<div className="search-bar">
+				
+				<input
+					value = {this.state.term}
+					onChange={(event) => this.onInputChange(event.target.value)} 
+				/>
+				
+			</div>
+		);
+
+	}
+	
+	onInputChange(term) {
+		this.setState({ term })
+		this.props.onSearchTermChange(term);
 	}
 }
 
